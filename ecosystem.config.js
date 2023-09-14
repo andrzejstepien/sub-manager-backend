051b8d92c6c80854f3cf9ddfc7b322b4714f021d
Andrzej Stepien <andrzejjamesstepien@gmail.com>
2023-09-14 16:06:01 +0200

    simpler name for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name   : "sub-manager-backend",
+    name   : "subman",
     script : "./server.mjs",
     ignore_watch:["submissions","submissions-journal","test.db"]
   }]
